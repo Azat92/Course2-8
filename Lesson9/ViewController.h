4c480048c75c660bef5aa6828bf8a2a6dec63c8e
Azat Almeev <azat.almeev@gmail.com>
2015-10-29 21:35:59 +0300

    Initial Commit

---
+++ Lesson9/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson9
+//
+//  Created by Azat Almeev on 29.10.15.
+//  Copyright © 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
